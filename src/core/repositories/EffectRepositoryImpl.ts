67ccce202b279fa3e6d90fdb0055afe75ebe45b9
Equindar <equindar@gmail.com>
2025-09-12 14:09:59 +0200

    some progress

--- src/core/repositories/EffectRepositoryImpl.ts
+++ src/core/repositories/EffectRepositoryImpl.ts
@@ -1,16 +1,24 @@
 import { Effect } from "../entities/effect";
+import { EffectDataSource } from "@/infrastructure/interfaces/EffectDataSource";
 import { EffectRepository } from "../interfaces/effect-repository";
 
 export class EffectRepositoryImpl implements EffectRepository {
+    effectDataSource: EffectDataSource;
+
+    constructor(effectDataSource: EffectDataSource) {
+        this.effectDataSource = effectDataSource;
+    }
+
     createEffect(effect: Effect): Promise<boolean> {
         throw new Error("Method not implemented.");
     }
+
     getEffect(): Promise<Effect> {
         throw new Error("Method not implemented.");
     }
+    
     getEffects(): Promise<Effect[]> {
         throw new Error("Method not implemented.");
     }
-
     
 }
